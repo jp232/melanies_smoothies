5fe5dc70429dfe63196997afe2c9865109bed23c
jp232 <162574971+jp232@users.noreply.github.com>
2024-03-07 16:14:16 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,4 +56,6 @@ if ingredients_list:
     st.success('Your Smoothie is ordered!')
 
 
-
+import requests 
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response.json())
